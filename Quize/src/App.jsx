4a34af1a6920b870e1091f289ebe7a192457a042
halwohamda <halwohamda@gmail.com>
2024-03-02 12:00:38 -0800

    done

--- Quize/src/App.jsx
+++ Quize/src/App.jsx
@@ -1,14 +1,14 @@
-// import React, { useState } from 'react'
-// import { answer } from './Types/Questions';
+import React, { useState } from 'react'
+import { answer } from './Types/Questions';
 
-// const App = () => {
-//   const [done,setDone] = useState(false);
-//   const [score,setScore] = useState(0);
-//  const [currentQuestion,setCurrentQuestion] = useState(0);
-//  const [answer,setAnswer] = useState<answerT[]>([]);
-//   return (
-//     <div className='text-red-700'>App</div>
-//   )
-// }
+const App = () => {
+  const [done,setDone] = useState(false);
+  const [score,setScore] = useState(0);
+ const [currentQuestion,setCurrentQuestion] = useState(0);
+ const [answer,setAnswer] = useState<answerT[]>([]);
+  return (
+    <div className='text-red-700'>App</div>
+  )
+}
 
-// export default App
+export default App
