eab47e0c02fff4600e10564e7290e2f82c917c6a
halwohamda <halwohamda@gmail.com>
2024-03-02 12:03:17 -0800

    done

--- Quize/src/App.jsx
+++ Quize/src/App.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { answer } from './Types/Questions';
 
 const App = () => {
-  // const [done,setDone] = useState(false);
-  // const [score,setScore] = useState(0);
-//  const [currentQuestion,setCurrentQuestion] = useState(0);
-//  const [answer,setAnswer] = useState<answerT[]>([]);
+  const [done,setDone] = useState(false);
+  const [score,setScore] = useState(0);
+ const [currentQuestion,setCurrentQuestion] = useState(0);
+ const [answer,setAnswer] = useState<answerT[]>([]);
   return (
     <div className='text-red-700'>App</div>
   )
